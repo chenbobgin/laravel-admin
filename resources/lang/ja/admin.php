58bdc26d61e8b6c5e3c2e85ee1bf980b0d94921a
GXP\b.chen <b.chen@grooowth.co.jp>
2018-01-30 18:19:57 +0900

    update 日本語化ファイルを追加

---
+++ resources/lang/ja/admin.php
@@ -0,0 +1,112 @@
+<?php
+
+return [
+    'online'                => 'オンライン',
+    'login'                 => 'ログイン',
+    'logout'                => 'ログアウト',
+    'setting'               => '設定',
+    'name'                  => '名前',
+    'username'              => 'ユーザーネーム',
+    'password'              => 'パスワード',
+    'password_confirmation' => 'パスワード（確認）',
+    'remember_me'           => 'ログイン情報を記憶',
+    'user_setting'          => 'ユーザー設定',
+    'avatar'                => 'アバター',
+
+    'list'         => '一覧',
+    'new'          => '新規作成する',
+    'create'       => 'Create',
+    'delete'       => '削除する',
+    'remove'       => '削除する',
+    'edit'         => '編集する',
+    'view'         => '表示する',
+    'browse'       => 'ブラウズ',
+    'reset'        => 'リセット',
+    'export'       => 'エクスポート',
+    'batch_delete' => '一括削除',
+    'save'         => '保存する',
+    'refresh'      => '更新',
+    'order'        => '順番',
+    'expand'       => '開く',
+    'collapse'     => '閉じる',
+    'filter'       => 'フィルタ',
+    'close'        => '閉じる',
+    'show'         => 'Show',
+    'entries'      => 'entries',
+    'captcha'      => 'Captcha',
+
+    'action'            => 'アクション',
+    'title'             => 'タイトル',
+    'description'       => '詳細',
+    'back'              => '戻る',
+    'back_to_list'      => '一覧に戻る',
+    'submit'            => '送信',
+    'menu'              => 'メニュー',
+    'input'             => '入力してください',
+    'succeeded'         => '成功しました',
+    'failed'            => '失敗しました',
+    'delete_confirm'    => '削除してよろしいでしょうか？',
+    'delete_succeeded'  => '削除しました！',
+    'delete_failed'     => '削除失敗',
+    'update_succeeded'  => '更新しました！',
+    'save_succeeded'    => '保存しました！',
+    'refresh_succeeded' => '更新しました！',
+    'login_successful'  => 'ログインしました！',
+
+    'choose'       => 'Select',
+    'choose_file'  => 'Select file',
+    'choose_image' => 'Select image',
+
+    'more' => 'もっと見る',
+    'deny' => 'アクセス拒否',
+
+    'administrator' => 'Administrator',
+    'roles'         => '役割',
+    'permissions'   => '権限',
+    'slug'          => 'Slug',
+
+    'created_at' => '作成日時',
+    'updated_at' => '更新日時',
+
+    'alert' => 'Alert',
+
+    'parent_id' => 'Parent',
+    'icon'      => 'Icon',
+    'uri'       => 'URI',
+
+    'operation_log'       => '操作ログ',
+    'parent_select_error' => 'Parent select error',
+
+    'pagination' => [
+        'range' => 'Showing :first to :last of :total entries',
+    ],
+
+    'role'       => '役割',
+    'permission' => '権限',
+    'route'      => 'Route',
+    'confirm'    => '確認',
+    'cancel'     => 'キャンセル',
+
+    'http' => [
+        'method' => 'HTTP method',
+        'path'   => 'HTTP path',
+    ],
+    'all_methods_if_empty' => 'All methods if empty',
+
+    'all'           => '全部',
+    'current_page'  => 'Current page',
+    'selected_rows' => 'Selected rows',
+
+    'upload'     => 'アップロード',
+    'new_folder' => 'フォルダーを作る',
+    'time'       => '時間',
+    'size'       => 'サイズ',
+
+    'listbox' => [
+        'text_total'         => '全部 {0}',
+        'text_empty'         => 'Empty list',
+        'filtered'           => '{0} / {1}',
+        'filter_clear'       => 'Show all',
+        'filter_placeholder' => 'フィルタ',
+    ],
+];
